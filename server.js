ae69cf5afb3ee5d0960b119eaa42b7146ef19394
MatVD <vandammemathieu@hotmail.fr>
2022-08-12 17:54:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.post('/', (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
+      user: "mathieuvd64@gmail.com",
+      pass: "ykjpfsoauygfplot"
     }
   })
 
