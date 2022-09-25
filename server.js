1dd8bc1fa7adc422f5a72a788ca6998a8aacc657
MatVD <vandammemathieu@hotmail.fr>
2022-09-25 21:48:49 +0200

    change on form background color

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ const PORT = process.env.PORT || 8000;
 app.use(express.static('public'));
 app.use(express.json());
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 });
